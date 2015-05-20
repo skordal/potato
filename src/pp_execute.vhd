229441a464783d4d37b583339ada110294d24d68
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2015-05-20 15:46:17 +0000

    Prevent exceptions from being taken while stalling
    Jumping to an exception handler while stalling and waiting for a load/store
    instruction to finish can cause undefined results from the load/store
    instruction. This actually fixes the issue mentioned in revision r20.

--- src/pp_execute.vhd
+++ src/pp_execute.vhd
@@ -199,7 +199,7 @@ begin
 	evec_out <= evec_forwarded;
 	exception_taken <= decode_exception or to_std_logic(exception_cause /= CSR_CAUSE_NONE) or irq_asserted; 
 
-	irq_asserted <= to_std_logic(exception_context_forwarded.status.ei = '1' and
+	irq_asserted <= (not stall) and to_std_logic(exception_context_forwarded.status.ei = '1' and
 		(irq and exception_context_forwarded.status.im) /= x"00");
 
 	rs1_data <= rs1_data_in;
