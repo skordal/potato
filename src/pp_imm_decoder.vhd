5dcd838429ab68d68c2b4b31ce511ba09152c6ca
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2018-03-22 17:49:41 +0100

    Update immediate value format names

--- src/pp_imm_decoder.vhd
+++ src/pp_imm_decoder.vhd
@@ -20,11 +20,11 @@ begin
 		case instruction(6 downto 2) is
 			when b"01101" | b"00101" => -- U type
 				immediate <= instruction(31 downto 12) & (11 downto 0 => '0');
-			when b"11011" => -- UJ type
+			when b"11011" => -- J type
 				immediate <= (31 downto 20 => instruction(31)) & instruction(19 downto 12) & instruction(20) & instruction(30 downto 21) & '0';
 			when b"11001" | b"00000" | b"00100"  | b"11100"=> -- I type
 				immediate <= (31 downto 11 => instruction(31)) & instruction(30 downto 20);
-			when b"11000" => -- SB type
+			when b"11000" => -- B type
 				immediate <= (31 downto 12 => instruction(31)) & instruction(7) & instruction(30 downto 25) & instruction(11 downto 8) & '0';
 			when b"01000" => -- S type
 				immediate <= (31 downto 11 => instruction(31)) & instruction(30 downto 25) & instruction(11 downto 7);
